1251ae3fd6f76cac79de411161ab94f7c5a833af
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-06-02 11:57:59 -0300

    feat: Novos components * Component navbar finalizado. * Component primary-buton criado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './_components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'gerador-certificado';
+
+  exibeNavBar: boolean = true;
 }
+
