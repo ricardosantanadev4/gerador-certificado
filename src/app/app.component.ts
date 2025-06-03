9f834a4b923fae452c694848403e2488e41f6b29
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-06-03 17:46:51 -0300

    feat: Pagina de fomulário de certificado criada.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BaseUiComponent } from './_components/base-ui/base-ui.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
-import { CertificadosComponent } from './pages/certificados/certificados.component';
+import { CertificadoFormComponent } from './pages/certificado-form/certificado-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, CommonModule, BaseUiComponent, CertificadosComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule, BaseUiComponent, CertificadoFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
