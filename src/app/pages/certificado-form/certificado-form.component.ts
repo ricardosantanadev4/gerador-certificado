9d179bb13ddee426d95ea0da9aad026394402abc
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-06-06 15:31:20 -0300

    feat: Validações de formulário adicionadas.

--- src/app/pages/certificado-form/certificado-form.component.ts
+++ src/app/pages/certificado-form/certificado-form.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButtonComponent, PrimaryButtonComponent],
+  imports: [SecondaryButtonComponent, PrimaryButtonComponent, FormsModule, CommonModule],
   templateUrl: './certificado-form.component.html',
   styleUrl: './certificado-form.component.css'
 })
@@ -12,4 +14,8 @@ export class CertificadoFormComponent {
   nome: string = '';
   atividade: string = '';
   atividades: string[] = ['React', 'Angular', 'Vue'];
+
+  campoValido(control: NgModel) {
+    return control.invalid && control.touched;
+  }
 }
