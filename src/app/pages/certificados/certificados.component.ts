363136316be638026c60fd2190daae8d3d091351
ricardosantanadev4 <ricardosantanadev4@gmail.com>
2025-06-08 11:39:30 -0300

    feat: Adicionado o decordor @Input nos botões para torná-los dinâmicos.

--- src/app/pages/certificados/certificados.component.ts
+++ src/app/pages/certificados/certificados.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { ItemCertificadoComponent } from '../../_components/item-certificado/item-certificado.component';
+import { RouterLink } from '@angular/router';
+import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
 
 @Component({
   selector: 'app-certificados',
-  imports: [ItemCertificadoComponent],
+  imports: [SecondaryButtonComponent, RouterLink],
   templateUrl: './certificados.component.html',
   styleUrl: './certificados.component.css'
 })
